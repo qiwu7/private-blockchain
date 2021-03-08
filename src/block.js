1bbe581f51c716f5341be3a6c3029f75efec2fb8
Qi Wu <qi.wu@coinbase.com>
2021-03-08 12:43:24 -0800

    Block (#1)
    * Implement validate
    * Implement getBData

--- src/block.js
+++ src/block.js
@@ -1,11 +1,11 @@
 /**
  *                          Block class
- *  The Block class is a main component into any Blockchain platform, 
+ *  The Block class is a main component into any Blockchain platform,
  *  it will store the data and act as a dataset for your application.
  *  The class will expose a method to validate the data... The body of
  *  the block will contain an Object that contain the data to be stored,
  *  the data should be stored encoded.
- *  All the exposed methods should return a Promise to allow all the methods 
+ *  All the exposed methods should return a Promise to allow all the methods
  *  run asynchronous.
  */
 
@@ -18,11 +18,11 @@ class Block {
 	constructor(data){
 		this.hash = null;                                           // Hash of the block
 		this.height = 0;                                            // Block Height (consecutive number of each block)
-		this.body = Buffer(JSON.stringify(data)).toString('hex');   // Will contain the transactions stored in the block, by default it will encode the data
+		this.body = Buffer.from(JSON.stringify(data)).toString('hex');   // Will contain the transactions stored in the block, by default it will encode the data
 		this.time = 0;                                              // Timestamp for the Block creation
 		this.previousBlockHash = null;                              // Reference to the previous Block Hash
     }
-    
+
     /**
      *  validate() method will validate if the block has been tampered or not.
      *  Been tampered means that someone from outside the application tried to change
@@ -38,35 +38,73 @@ class Block {
     validate() {
         let self = this;
         return new Promise((resolve, reject) => {
-            // Save in auxiliary variable the current block hash
-                                            
-            // Recalculate the hash of the Block
-            // Comparing if the hashes changed
-            // Returning the Block is not valid
-            
-            // Returning the Block is valid
-
+            try {
+                // Save in auxiliary variable the current block hash
+                const currentHash = self.hash;
+                // Recalculate the hash of the Block
+                const recalculatedHash = this.calculateHash();
+                // Comparing if the hashes changed
+                // Returning the Block is not valid
+                // Returning the Block is valid
+                resolve(currentHash === recalculatedHash);
+            } catch (e) {
+                reject(e);
+            }
         });
     }
 
     /**
      *  Auxiliary Method to return the block body (decoding the data)
      *  Steps:
-     *  
+     *
      *  1. Use hex2ascii module to decode the data
      *  2. Because data is a javascript object use JSON.parse(string) to get the Javascript Object
-     *  3. Resolve with the data and make sure that you don't need to return the data for the `genesis block` 
+     *  3. Resolve with the data and make sure that you don't need to return the data for the `genesis block`
      *     or Reject with an error.
      */
     getBData() {
-        // Getting the encoded data saved in the Block
-        // Decoding the data to retrieve the JSON representation of the object
-        // Parse the data to an object to be retrieve.
+        let self = this;
+        return new Promise((resolve, reject) => {
+            try {
+                if (this.isGenesis()) {
+                    reject(new Error("Cannot decode Genesis Block."));
+                }
 
-        // Resolve with the data if the object isn't the Genesis block
-
+                // Getting the encoded data saved in the Block
+                const encodedData = self.body;
+                // Decoding the data to retrieve the JSON representation of the object
+                const decodedData = hex2ascii(encodedData);
+                // Parse the data to an object to be retrieve.
+                const jsonObj = JSON.parse(decodedData);
+                // Resolve with the data if the object isn't the Genesis block
+                resolve(jsonObj);
+            } catch (e) {
+                reject(e);
+            }
+        });
     }
 
+    /**
+     * Calcualte the hash of the block based on height, body
+     * timestamp and the previous block hash.
+     */
+    calculateHash() {
+        return SHA256(
+            JSON.stringify({
+                height: this.height,
+                body: this.body,
+                time: this.time,
+                previousHash: this.previousBlockHash,
+            })
+        ).toString();
+    }
+
+    /**
+     * Determine if the the block is genesis block
+     */
+    isGenesis() {
+        return this.height === 0;
+    }
 }
 
 module.exports.Block = Block;                    // Exposing the Block class as a module
